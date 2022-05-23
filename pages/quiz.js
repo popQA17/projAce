0a20256748a5d685ab27323ce830846022db3be8
Pop Plays <85472155+popQA17@users.noreply.github.com>
2022-05-23 08:39:34 +0000

    some stuff

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,12 +1,15 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import {Box, Text, Button, Heading} from '@chakra-ui/react'
 export default function Quiz(){
     const router = useRouter();
-    useEffect(()=>{
-        window.open('https://quiz.popplays.tk')
-    }, [])
     return(
         <>
+          <Box display='flex' alignItems='center' justifyContent='center' flexDirection='column'>
+            <Heading>Quiz Time!</Heading>
+            <Text>The button below will bring you to an external quiz site.</Text>
+            <Button onClick={()=> window.open('https://quiz.popplays.tk')}>Start</Button>
+          </Box>
         </>
     )
 }
